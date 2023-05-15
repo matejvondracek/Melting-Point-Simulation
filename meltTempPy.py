b2887b40b621ed6aded777b59c71cd2082648b33
matej.vondracek <matej.vondr.2002@gmail.com>
2023-05-15 17:48:28 +0200

    first commit

---
+++ meltTempPy.py
@@ -0,0 +1,117 @@
+from lammps import lammps
+import time
+
+class MeltTempPy:
+    system = "" 
+    styles = ""
+    potentials = ""
+    thermo = ""
+    name = ""
+    compute = """
+            compute pe all pe
+            compute temp all temp
+            """
+    timestep = 0.0
+    run = 0
+    t = 0
+    n_atom_types = 0
+
+
+    def __init__(self, system, styles, potentials, thermo, name, n_atom_types):
+        self.system = system
+        self.styles = styles
+        self.potentials = potentials
+        self.thermo = thermo
+        self.name += name
+        self.n_atom_types = n_atom_types
+
+
+    def _run_sim_init(self, source, t_min, t_max, create_box_settings):
+        # melting for liquid
+        lmp = lammps()
+        lmp.commands_string(self.system + self.styles)
+        lmp.command(f"read_data {source}")
+        lmp.commands_string(self.potentials + self.thermo + self.compute) 
+        s = f"""
+            velocity all create {t_max} 123456 dist gaussian
+            fix f1 all nvt temp {t_max} {t_max} 10.0
+            dump d all atom 10 {self.name}_init_melt.dump
+            
+            timestep {self.timestep}
+            run {self.run}
+
+            write_data temp.lmp pair ij 
+            """
+        lmp.commands_string(s)
+
+        boxlo, boxhi, xy, yz, zx, per, type = lmp.extract_box()
+
+        # combining liquid and solid
+        lmp = lammps()
+        lmp.commands_string(self.system + self.styles)
+        lmp.command(f"region sim_box block 0 {2 * (boxhi[0] + boxlo[0])} 0 {boxhi[1] + boxlo[1]} 0 {boxhi[0] + boxlo[0]}")
+        lmp.command(f"create_box {self.n_atom_types} sim_box {create_box_settings}")
+        
+        lmp.command(f"read_data temp.lmp add append group liquid shift {boxlo[0]} {boxlo[1]} {boxlo[2]}")
+        lmp.command(f"read_data {source} add append group solid shift {2 * boxlo[0] + boxhi[0]} {boxlo[1]} {boxlo[2]}")
+        
+        lmp.commands_string(self.potentials)
+        lmp.commands_string(self.thermo + self.compute)
+        s = f"""
+            velocity solid create 1 123456 dist gaussian
+            fix f1 solid nvt temp 1 1 1000.0
+            dump d all atom 10 {self.name}_init_solid_equi.dump
+            run {self.run}
+
+            unfix f1            
+            velocity all create 1 123456 dist gaussian
+            fix f2 all npt temp 1 1 1000.0 iso 1.0 1.0 1000.0
+
+            run 10
+            
+            write_data {self.name}_init.lmp pair ij
+            """
+        lmp.commands_string(s)
+        return lmp.extract_compute("pe", 0, 0)
+    
+    
+    def _run_sim_nvt(self, temp):
+        lmp = lammps()
+        lmp.commands_string(self.system + self.styles)
+        lmp.command(f"read_data {self.name}_init.lmp")
+        lmp.commands_string(self.potentials + self.thermo + self.compute)
+        s = f"""
+            velocity all create 1 123456 dist gaussian
+            fix f1 all nvt temp 1 {temp} 10.0
+            dump d all atom 10 {self.name}_nvt_{int(temp)}.dump
+
+            timestep {self.timestep}
+            run {self.run}   
+            """
+        lmp.commands_string(s)
+        return lmp.extract_compute("pe", 0, 0)
+    
+    
+    def find_tmelt(self, source, time_step, steps, t_min, t_max, max_dev, create_box_settings):
+        start_time = time.time()
+        self.timestep = time_step
+        self.run = steps
+
+        pe_init = self._run_sim_init(source, t_min, t_max, create_box_settings)
+
+        while (t_max - t_min > 2 * max_dev):
+            t_mid = (t_max + t_min) / 2
+            print(t_mid)
+            pe_res = self._run_sim_nvt(t_mid)
+            if pe_res is not None:       
+                if (pe_init > pe_res):
+                    t_min = t_mid
+                else:
+                    t_max = t_mid 
+            else:
+                Exception()
+            
+        t_mid = (t_max + t_min) / 2
+        dev = (t_max - t_min) / 2
+        self.t = round((time.time() - start_time) / 60)
+        return t_mid, dev
---
+++ waterTemp.py
@@ -0,0 +1,37 @@
+from meltTempPy import MeltTempPy
+
+system = """
+    units      real
+    dimension  3
+    boundary   p p p
+    atom_style full
+    """
+
+styles = """
+    pair_style lj/cut/coul/long 10.0
+    bond_style  harmonic
+    angle_style harmonic
+    kspace_style pppm 1.0e-5 
+    """
+
+potentials = """
+    group ox type 2
+    group hy type 1
+
+    set group ox charge -0.830
+    set group hy charge 0.415
+
+    pair_coeff 1 1*2 0.000 0.000 
+    pair_coeff 2 2 0.102 3.188 
+    bond_coeff  1 450 0.9572
+    angle_coeff 1 55 104.52
+    """
+
+thermo = """
+    thermo      100
+    thermo_style custom step temp pe
+    """
+
+mtp = MeltTempPy(system, styles, potentials, thermo, "H2O", 2)
+temp, dev = mtp.find_tmelt("Ice-Ic-40A-box.dat", 0.5, 3000, 100, 400, 2, "bond/types 1 angle/types 1 extra/bond/per/atom 1000 extra/angle/per/atom 1000")
+print(f"Melting temperature of water is {temp} ± {dev} K")
